559feab9bf4b343137eb5b2fdebe0dc54dbe5ce5
Will Sturgeon <willstrgn@gmail.com>
2025-04-29 15:32:03 -0700

    Rapidly capture small JPEGs

---
+++ main/camera_pins.h
@@ -0,0 +1,28 @@
+#define CAM_PIN_PWDN     -1
+#define CAM_PIN_RESET    -1
+#define CAM_PIN_XCLK     10
+#define CAM_PIN_SIOD     40
+#define CAM_PIN_SIOC     39
+
+#define CAM_PIN_Y9       48
+#define CAM_PIN_Y8       11
+#define CAM_PIN_Y7       12
+#define CAM_PIN_Y6       14
+#define CAM_PIN_Y5       16
+#define CAM_PIN_Y4       18
+#define CAM_PIN_Y3       17
+#define CAM_PIN_Y2       15
+#define CAM_PIN_VSYNC    38
+#define CAM_PIN_HREF     47
+#define CAM_PIN_PCLK     13
+
+#define CAM_PIN_LED      21
+
+#define CAM_PIN_D7 CAM_PIN_Y9
+#define CAM_PIN_D6 CAM_PIN_Y8
+#define CAM_PIN_D5 CAM_PIN_Y7
+#define CAM_PIN_D4 CAM_PIN_Y6
+#define CAM_PIN_D3 CAM_PIN_Y5
+#define CAM_PIN_D2 CAM_PIN_Y4
+#define CAM_PIN_D1 CAM_PIN_Y3
+#define CAM_PIN_D0 CAM_PIN_Y2
