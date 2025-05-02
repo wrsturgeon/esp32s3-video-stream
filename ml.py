dbdd235d7cac274b758add0990b1ef064d60871c
Will Sturgeon <willstrgn@gmail.com>
2025-05-02 14:46:45 -0700

    maybe CUDA image allocation and `memcpy`ing?

--- ml.py
+++ ml.py
@@ -5,6 +5,8 @@ import jetson_utils
 
 import cv2
 
+full_jpeg_buffer = None
+
 def show(im):
     cv2.imshow('Livestream', im)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -13,5 +15,13 @@ def show(im):
         sys.exit(0)
 
 def process(im):
-    print(f"im: {im}")
+    if full_jpeg_buffer is None:
+        full_jpeg_buffer = jetson_utils.cudaImage(like=im) # jetson_utils.cudaImage(width=160, height=120, format="rgb8")
+
+    # Seems to be (height, width, channels), both in `im` (from OpenCV) and in CUDA.
+    jetson_utils.cudaMemcpy(dst=full_jpeg_buffer, src=im)
+
+    # Wait for the GPU to finish processing shared memory:
+    jetson_utils.cudaDeviceSynchronize()
+
     exit()
