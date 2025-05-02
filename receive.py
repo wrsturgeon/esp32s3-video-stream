be5400201b86525d9438abab907f1dd1f457bb38
Will Sturgeon <willstrgn@gmail.com>
2025-05-02 12:16:19 -0700

    Actually correct the above

--- receive.py
+++ receive.py
@@ -27,27 +27,7 @@ while True:
     for i in range(0, total_chunks):
         data = packet[HEADER_SIZE:]
         data_len = len(data)
-        if i < total_chunks - 1:
-            if data_len != CHUNK_SIZE:
-                print(f"Invalid data size (expected {CHUNK_SIZE} but found {data_len}) for chunk #{i}/{total_chunks} of frame #{frame_id}. Skipping.")
-                break
-            jpeg_buffer[(CHUNK_SIZE * i):(CHUNK_SIZE * (i + 1))] = data
-
-            packet, addr = sock.recvfrom(2048)
-            this_frame_id, chunk_id, this_total_chunks = struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
-            if this_frame_id != frame_id:
-                print(f"Lost frame #{frame_id} ({i}/{total_chunks}); continuing on frame #{this_frame_id}.")
-                frame_id = this_frame_id
-                total_chunks = this_total_chunks
-                break
-            if this_total_chunks != total_chunks:
-                print(f"Different total chunks for frame #{total_chunks} (was {total_chunks}, now {this_total_chunks}). Skipping.")
-                break
-            if chunk_id != i:
-                print(f"In frame #{frame_id}, expected chunk #{i} but got chunk #{chunk_id}.")
-                break
-
-        else:
+        if i == total_chunks - 1:
             if data_len > CHUNK_SIZE:
                 print(f"Invalid data size (expected <={CHUNK_SIZE} but found {data_len}) for chunk #{i}/{total_chunks} of frame #{frame_id}. Skipping.")
                 break
@@ -57,3 +37,24 @@ while True:
             arr = np.asarray(jpeg_buffer, dtype="uint8")
             im = cv2.imdecode(arr, cv2.IMREAD_COLOR)
             cv2.imshow('Livestream', im)
+
+            break
+
+        if data_len != CHUNK_SIZE:
+            print(f"Invalid data size (expected {CHUNK_SIZE} but found {data_len}) for chunk #{i}/{total_chunks} of frame #{frame_id}. Skipping.")
+            break
+        jpeg_buffer[(CHUNK_SIZE * i):(CHUNK_SIZE * (i + 1))] = data
+
+        packet, addr = sock.recvfrom(2048)
+        this_frame_id, chunk_id, this_total_chunks = struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
+        if this_frame_id != frame_id:
+            print(f"Lost frame #{frame_id} ({i}/{total_chunks}); continuing on frame #{this_frame_id}.")
+            frame_id = this_frame_id
+            total_chunks = this_total_chunks
+            break
+        if this_total_chunks != total_chunks:
+            print(f"Different total chunks for frame #{total_chunks} (was {total_chunks}, now {this_total_chunks}). Skipping.")
+            break
+        if chunk_id != i:
+            print(f"In frame #{frame_id}, expected chunk #{i} but got chunk #{chunk_id}.")
+            break
