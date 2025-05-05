c29abe69dab3a9f3f6e6cbb7c761525e24cbaf3a
Will Sturgeon <willstrgn@gmail.com>
2025-05-05 15:35:11 -0700

    Display only relevant face lines (optionally)

--- ml.py
+++ ml.py
@@ -21,11 +21,15 @@ DLIB_FACE_DETECTOR = dlib.get_frontal_face_detector()
 DLIB_LANDMARK_PREDICTOR = dlib.shape_predictor(DLIB_LANDMARK_PREDICTOR_PATH)
 
 SCALE_UP_BEFORE_DETECTING_FACES = 0
-LOG_DISPLAY_UPSCALE = 3
+LOG_DISPLAY_UPSCALE = 2
 
 FACE_BBOX = None
 FACE_BBOX_LAST_UPDATE = None
 FACE_BBOX_UPDATE_PERIOD_SECONDS = 0.5
+
+DISPLAY_FACE_BBOX = True
+DISPLAY_ALL_FACE_POINTS = False
+DISPLAY_RELEVANT_FACE_LINES = True
 
 def show(im):
     cv2.imshow('Livestream', im)
@@ -78,17 +82,35 @@ def process(im):
 
     im = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
 
-    x, y = FACE_BBOX.left() * multiplier, FACE_BBOX.top() * multiplier
-    w = (FACE_BBOX.right() * multiplier) - x
-    color = (0, int(255. * (1. - face_bbox_staleness)), int(255. * face_bbox_staleness))
-    cv2.rectangle(im, (x, y), (FACE_BBOX.right() * multiplier, FACE_BBOX.bottom() * multiplier), color, (w + 511) // 512)
-    cv2.putText(im, "Face", (x, y - ((w + 127) // 128)), cv2.FONT_HERSHEY_SIMPLEX, w / 512., color, (w + 511) // 512)
+    if DISPLAY_FACE_BBOX:
+        x, y = FACE_BBOX.left() * multiplier, FACE_BBOX.top() * multiplier
+        w = (FACE_BBOX.right() * multiplier) - x
+        color = (0, int(255. * (1. - face_bbox_staleness)), int(255. * face_bbox_staleness))
+        cv2.rectangle(im, (x, y), (FACE_BBOX.right() * multiplier, FACE_BBOX.bottom() * multiplier), color, (w + 511) // 512)
+        cv2.putText(im, "Face", (x, y - ((w + 127) // 128)), cv2.FONT_HERSHEY_SIMPLEX, w / 512., color, (w + 511) // 512)
 
-    for i, point in enumerate(landmarks.parts()):
-        x = point.x * multiplier
-        y = point.y * multiplier
-        m = (multiplier + 1) // 2 # `+ 1` just so this is not 0 when m = 1
-        cv2.circle(im, (x, y), m, (0, 0, 255), -1)
-        cv2.putText(im, f"{i}", (x, y - m), cv2.FONT_HERSHEY_SIMPLEX, w / 1024., (0, 255, 0), (w + 1023) // 1024)
+    if DISPLAY_ALL_FACE_POINTS:
+        for i, point in enumerate(landmarks.parts()):
+            x = point.x * multiplier
+            y = point.y * multiplier
+            m = (multiplier + 1) // 2 # `+ 1` just so this is not 0 when m = 1
+            cv2.circle(im, (x, y), m, (255, 0, 0), -1)
+            cv2.putText(im, f"{i}", (x, y - m), cv2.FONT_HERSHEY_SIMPLEX, w / 1024., (0, 255, 0), (w + 1023) // 1024)
+
+    if DISPLAY_RELEVANT_FACE_LINES:
+
+        eyebrow_left_left = landmarks.part(18)
+        eyebrow_left_center = landmarks.part(19)
+        eyebrow_left_right = landmarks.part(20)
+
+        eyebrow_right_left = landmarks.part(23)
+        eyebrow_right_center = landmarks.part(24)
+        eyebrow_right_right = landmarks.part(25)
+
+        cv2.line(im, (eyebrow_left_left.x, eyebrow_left_left.y), (eyebrow_left_center.x, eyebrow_left_center.y), (255, 0, 0), (w + 511) // 512)
+        cv2.line(im, (eyebrow_left_center.x, eyebrow_left_center.y), (eyebrow_left_right.x, eyebrow_left_right.y), (255, 0, 0), (w + 511) // 512)
+
+        cv2.line(im, (eyebrow_right_left.x, eyebrow_right_left.y), (eyebrow_right_center.x, eyebrow_right_center.y), (255, 0, 0), (w + 511) // 512)
+        cv2.line(im, (eyebrow_right_center.x, eyebrow_right_center.y), (eyebrow_right_right.x, eyebrow_right_right.y), (255, 0, 0), (w + 511) // 512)
 
     show(im)
